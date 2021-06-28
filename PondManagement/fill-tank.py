4eeeb38b145154e9abb88a3d2fd5d8f5e49d9977
Henry Groover <henry.groover@gmail.com>
2021-06-28 05:09:02 -0500

    Add temp/humidity reading to fill tank

--- PondManagement/fill-tank.py
+++ PondManagement/fill-tank.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 import serial
 import sys
+# Comment out the following two lines if DHT11 not connected to pi
+import Adafruit_DHT
+sensor = Adafruit_DHT.DHT11
+# Set to 0 if DHT11 not connected, or GPIO pin
+dht11_gpio = 4
 
 if __name__=='__main__':
   ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-  ser.flush()
+  #ser.flush()
 
   # Start fill via f, wait for FILCOMP then FPH
   sent = 0
   got_fph = 0
   post_fph_count = 0
+  got_temperature = 0
+  post_fph_count = 5000
+  if dht11_gpio != 0:
+     # Worst-case of immediate completion and unresponsive DHT11 will be about 30s
+     post_fph_count = 10
   outstr = "f"
   while True:
     if ser.in_waiting > 0:
@@ -25,4 +35,9 @@ if __name__=='__main__':
         post_fph_count = post_fph_count + 1
         if post_fph_count > 5000:
           sys.exit(0)
-
+      if dht11_gpio != 0 and got_temperature == 0:
+        # Default retries is 15
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, dht11_gpio, retries=2, delay_seconds=2)
+        if humidity is not None and temperature is not None:
+          got_temperature = 1
+          print('Temperature: {0:0.1f}C  Humidity: {1:0.1f}%'.format(temperature, humidity))
