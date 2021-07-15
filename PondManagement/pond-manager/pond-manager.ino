9514add2b1d1abad773885ce67a5d7c0959b41a3
Henry Groover <henry.groover@gmail.com>
2021-07-15 15:59:23 -0500

    Added options and 1 (aux1) and 2 (aux2) commands. Commands are now sent terminated with LF (\n)

--- PondManagement/pond-manager/pond-manager.ino
+++ PondManagement/pond-manager/pond-manager.ino
@@ -1,5 +1,6 @@
 // Pond management - this manages a barrel with an input valve (24VAC), a discharge pump (120VAC), a high water float sensor (circuit open when barrel full),
-// and a low water float sensor (circuit open when barrel not empty).
+// and a low water float sensor (circuit open when barrel not empty). An additional 24VAC circuit (AUX1) can be used to control a turntable motor
+// for an automated fish feeder. The other auxiliary circuit controls 120VAC (AUX2) and is currently unused.
 // Commands come from connect Raspberry Pi, either
 // drain - run pump for time limit or until barrel empty
 // fill - fill barrel until time limit or barrel full
@@ -9,7 +10,9 @@ int pins[] = { 4, 5, 6, 7 };
 
 // Index assignments for relay pins
 const int rly_pump = 3;
+const int rly_aux2 = 2; // Unused 120VAC
 const int rly_valve = 0;
+const int rly_aux1 = 1; // 24VAC for fish feeder
 
 // Input pins. These are wired with a loop from +5V to the pin and on the pin side, through a 10k resistor to ground
 const int input_high = 8;
@@ -24,7 +27,19 @@ unsigned long last_active_display = 0;
 int force_display = 0;
 int force_pin_high = -1;
 
-int mode = 0; // 0 = idle, 1 = filling, 2 = draining; OR with 0x80 when edge reached
+// Line input buffer
+char line_buffer[16];
+int line_length = 0;
+
+// Option values
+int opt1_FTO = 540; // Was 480s
+int opt2_FPOST = 10; // Unchanged
+int opt3_DTO = 540; // Was 420s
+int opt4_DPOST = 20; // Unchanged
+int opt5_AUX1LEN = 2000; // ms
+int opt6_AUX2LEN = 2000; // ms
+
+int mode = 0; // 0 = idle, 1 = filling, 2 = draining, 3 = aux1 active, 4 = aux2 active; OR with 0x80 when edge reached
 
 void setup() {
   Serial.begin( 9600 );
@@ -35,16 +50,57 @@ void setup() {
   pinMode(input_low, INPUT);
 
   pinMode(pins[rly_pump], OUTPUT);
+  pinMode(pins[rly_aux1], OUTPUT);
   pinMode(pins[rly_valve], OUTPUT);
+  pinMode(pins[rly_aux2], OUTPUT);
   digitalWrite(pins[rly_pump], HIGH);
+  digitalWrite(pins[rly_aux1], HIGH);
   digitalWrite(pins[rly_valve], HIGH);
+  digitalWrite(pins[rly_aux2], HIGH);
   Serial.println("Setup complete");
 }
 
 void loop() {
+  // Check for available characters
   unsigned long curTime = millis();
-  int elapsed_s = (curTime - last_time) / 1000;
-  if (last_time != 0 && curTime >= last_time && elapsed_s < 1)
+  int input_chars = Serial.available();
+  int line_complete = 0;
+  if (input_chars > 0)
+  {
+    while (input_chars > 0 && line_length < 15 && !line_complete)
+    {
+      input_chars--;
+      int cmdIn = Serial.read();
+      switch (cmdIn)
+      {
+        case 0x08:
+          if (line_length > 0) {
+            line_length--;
+            line_buffer[line_length] = 0;
+          }
+          break;
+        case '\r':
+        case ' ':
+        case '\t':
+          // Silently ignore
+          break;
+        case '\n':
+          line_complete = 1;
+          break;
+        default:
+          line_buffer[line_length] = cmdIn;
+          line_length++;
+          line_buffer[line_length] = 0;
+          break;
+      }
+    }
+    //Serial.println("Complete:" + String(line_complete) + " len:" + String(line_length) + " >" + line_buffer);
+  } // Input available
+
+  int elapsed_m = curTime - last_time;
+  int elapsed_s = elapsed_m / 1000;
+  // Process only on second elapsed unless mode 3 or greater
+  if (last_time != 0 && curTime >= last_time && elapsed_s < 1 && line_complete == 0 && (mode & 0x7f) < 3)
   {
     return;
   }
@@ -55,7 +111,7 @@ void loop() {
   if (force_display != 0 || last_high_state != high_state || last_low_state != low_state)
   {
     force_display = 0;
-    Serial.println("@" + String(curTime/1000) + " M:" + String(mode) + " H:" + String(high_state) + " L:" + String(low_state));
+    Serial.println("@" + String(curTime / 1000) + " M:" + String(mode) + " H:" + String(high_state) + " L:" + String(low_state));
     last_high_state = high_state;
     last_low_state = low_state;
   }
@@ -63,54 +119,91 @@ void loop() {
   {
     Serial.println("FPH: " + String(force_pin_high));
     delay(50);
-    digitalWrite(pins[(force_pin_high+3)%4], HIGH);
+    digitalWrite(pins[(force_pin_high + 3) % 4], HIGH);
     delay(50);
-    digitalWrite(pins[(force_pin_high+2)%4], HIGH);
+    digitalWrite(pins[(force_pin_high + 2) % 4], HIGH);
     delay(50);
-    digitalWrite(pins[(force_pin_high+1)%4], HIGH);
+    digitalWrite(pins[(force_pin_high + 1) % 4], HIGH);
     delay(50);
-    digitalWrite(pins[(force_pin_high+0)%4], HIGH);
+    digitalWrite(pins[(force_pin_high + 0) % 4], HIGH);
     force_pin_high = -1;
   }
-  int input_chars = Serial.available();
-  if (input_chars > 0)
+  if (line_complete && line_length > 0)
   {
     int last_mode = mode;
-    while (input_chars > 0)
-    {
-      input_chars--;
-      int cmdIn = Serial.read();
-      switch (cmdIn)
-      {
-        case 'f':
+    // Commands are f, d, c, 1, 2, s
+    // Options are '$' <n> '=' <value>
+    switch (line_buffer[0]) {
+      case 'f':
           Serial.println("Starting fill");
           mode = 1;
           mode_start = millis();
           break;
-        case 'd':
+      case 'd':
           Serial.println("Starting drain");
           mode = 2;
           mode_start = millis();
           break;
-        case 'c':
+      case 'c':
           Serial.println("Cancelling");
-          mode = 0;  
+          mode = 0;
           force_display = 1;
           delay(50);
           digitalWrite(pins[rly_pump], HIGH);
           delay(50);
           digitalWrite(pins[rly_valve], HIGH);
           break;
-        case '\r':
-        case '\n':
-        case ' ':
-        case '\t':
-          // Silently ignore
+      case '1':
+          Serial.println("Starting aux1");
+          mode = 3;
+          force_display = 1;
+          mode_start = millis();
           break;
-        default:
-          Serial.println("Ignoring: " + cmdIn);
+      case '2':
+          Serial.println("Starting aux2");
+          mode = 4;
+          force_display = 1;
+          mode_start = millis();
           break;
-      }
+      case 's':
+          force_display = 1;
+          break;
+      // Handle options
+      case '$':
+          if (line_length < 5)
+          {
+            Serial.println("Invalid opt fmt");
+          }
+          else
+          {
+            switch (line_buffer[1]) {
+              case '1': // FTO
+                opt1_FTO = int(&line_buffer[3]);
+                break;
+              case '2': // FPOST
+                opt2_FPOST = int(&line_buffer[3]);
+                break;
+              case '3': // DTO
+                opt3_DTO = int(&line_buffer[3]);
+                break;
+              case '4': // DPOST
+                opt4_DPOST = int(&line_buffer[3]);
+                break;
+              case '5': // AUX1LEN
+                opt5_AUX1LEN = int(&line_buffer[3]);
+                break;
+              case '6': // AUX2LEN
+                opt6_AUX2LEN = int(&line_buffer[3]);
+                break;
+              default:
+                Serial.println("Invalid opt");
+                break;
+            }
+          }
+          break;
+      default:
+        Serial.println("Invalid cmd");
+        break;
     }
     if (mode != last_mode)
     {
@@ -125,9 +218,19 @@ void loop() {
           delay(50);
           digitalWrite(pins[rly_valve], LOW);
           break;
+        case 3:
+          delay(50);
+          digitalWrite(pins[rly_aux1], LOW);
+          break;
+        case 4:
+          delay(50);
+          digitalWrite(pins[rly_aux2], LOW);
+          break;
       }
     }
   }
+  line_complete = 0;
+  line_length = 0;
   if (mode == 0)
   {
     return;
@@ -144,7 +247,7 @@ void loop() {
         last_active_display = edge_start;
       }
       // Maximum 8 minutes to fill
-      else if (mode_elapsed < 0 || mode_elapsed >= 480000)
+      else if (mode_elapsed < 0 || mode_elapsed >= opt1_FTO * 1000)
       {
         Serial.println("FTO elapsed " + String(mode_elapsed / 1000));
         delay(150);
@@ -166,7 +269,7 @@ void loop() {
         last_active_display = edge_start;
       }
       // Maximum 7 minutes to drain.
-      else if (mode_elapsed < 0 || mode_elapsed >= 420000)
+      else if (mode_elapsed < 0 || mode_elapsed >= opt3_DTO * 1000)
       {
         Serial.println("DTO elapsed " + String(mode_elapsed / 1000));
         delay(150);
@@ -179,12 +282,40 @@ void loop() {
         force_display = 1;
       }
       break;
+    case 3: // AUX1 
+      if (mode_elapsed >= opt5_AUX1LEN)
+      {
+         Serial.println("AUX1 complete " + String(mode_elapsed / 1000));
+         delay(150);
+         digitalWrite(pins[rly_aux1], HIGH);
+         delay(250);
+         digitalWrite(pins[rly_aux1], HIGH);
+         force_pin_high = rly_aux1;
+         last_active_display = millis();
+         mode = 0;
+         force_display = 1;
+      }
+      break;
+    case 4: // AUX2
+      if (mode_elapsed >= opt6_AUX2LEN)
+      {
+         Serial.println("AUX2 complete " + String(mode_elapsed / 1000));
+         delay(150);
+         digitalWrite(pins[rly_aux2], HIGH);
+         delay(250);
+         digitalWrite(pins[rly_aux2], HIGH);
+         force_pin_high = rly_aux2;
+         last_active_display = millis();
+         mode = 0;
+         force_display = 1;
+      }
+      break;
   }
   long edge_elapsed = millis() - edge_start;
   switch (mode)
   {
     case 0x81: // Filling, edge reached
-      if (edge_elapsed < 0 || edge_elapsed >= 10000)
+      if (edge_elapsed < 0 || edge_elapsed >= opt2_FPOST * 1000)
       {
         Serial.println("FILCOMP @" + String(mode_elapsed / 1000) + "," + String(edge_elapsed / 1000));
         last_active_display = millis();
@@ -198,7 +329,7 @@ void loop() {
       }
       break;
     case 0x82: // Draining, edge reached
-      if (edge_elapsed < 0 || edge_elapsed >= 20000)
+      if (edge_elapsed < 0 || edge_elapsed >= opt4_DPOST * 1000)
       {
         Serial.println("DRNCOMP @" + String(mode_elapsed / 1000) + "," + String(edge_elapsed / 1000));
         last_active_display = millis();
