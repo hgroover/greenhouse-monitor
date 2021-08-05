439480de13be5aed8cd0b1e5557b04201794dcef
Henry Groover <henry.groover@gmail.com>
2021-08-05 03:47:44 -0500

    For both drain and fill, take optional args for completion timeout and post-completion additional seconds.

--- PondManagement/drain-tank.py
+++ PondManagement/drain-tank.py
@@ -3,16 +3,27 @@ import serial
 import sys
 import time
 
+drain_timeout = 480
+drain_post = 10
+
 if __name__=='__main__':
   ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
   ser.flush()
 
   # Start drain via d, wait for DRNCOMP then FPH
   # Optionally send setup $3=<dto-s> and $4=<dpost-s>
+  if len(sys.argv) >= 2:
+    drain_timeout = int(sys.argv[1])
+    if len(sys.argv) >= 3:
+      drain_post = int(sys.argv[2])
+
   sent = 0
   got_fph = 0
   post_fph_count = 0
-  outstr = "d\n"
+  outstr1 = '$3={0}\n'.format(drain_timeout)
+  outstr2 = '$4={0}\n'.format(drain_post)
+  outstr3 = "d\n"
+
   start_time = time.clock_gettime(time.CLOCK_MONOTONIC)
   while True:
     if ser.in_waiting > 0:
@@ -25,8 +36,20 @@ if __name__=='__main__':
          elapsed = time.clock_gettime(time.CLOCK_MONOTONIC) - start_time
          if elapsed >= 3.0:
            sent = 1
-           print('Sending {0:s} at {1:0.3f}'.format(outstr, elapsed)) 
-           ser.write(outstr.encode())
+           print('Sending {0:s} at {1:0.3f}'.format(outstr1, elapsed)) 
+           ser.write(outstr1.encode())
+      if sent == 1:
+         elapsed = time.clock_gettime(time.CLOCK_MONOTONIC) - start_time
+         if elapsed >= 6.0:
+           sent = 2
+           print('Sending {0:s} at {1:0.3f}'.format(outstr2, elapsed))
+           ser.write(outstr2.encode())
+      if sent == 2:
+         elapsed = time.clock_gettime(time.CLOCK_MONOTONIC) - start_time
+         if elapsed >= 9.0:
+           sent = 3
+           print('Sending {0:s} at {1:0.3f}'.format(outstr3, elapsed))
+           ser.write(outstr3.encode())
       if got_fph:
         post_fph_count = post_fph_count + 1
         if post_fph_count > 5000:
--- PondManagement/fill-tank.py
+++ PondManagement/fill-tank.py
@@ -8,6 +8,9 @@ sensor = Adafruit_DHT.DHT11
 # Set to 0 if DHT11 not connected, or GPIO pin
 dht11_gpio = 4
 
+fill_timeout = 480
+fill_post = 10
+
 if __name__=='__main__':
   ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
   #ser.flush()
@@ -22,7 +25,9 @@ if __name__=='__main__':
   if dht11_gpio != 0:
      # Worst-case of immediate completion and unresponsive DHT11 will be about 30s
      post_fph_count = 10
-  outstr = "f\n"
+  outstr1 = '$1={0}\n'.format(fill_timeout)
+  outstr2 = '$2={0}\n'.format(fill_post)
+  outstr3 = "f\n"
   start_time = time.clock_gettime(time.CLOCK_MONOTONIC)
   while True:
     if ser.in_waiting > 0:
@@ -35,8 +40,20 @@ if __name__=='__main__':
          elapsed = time.clock_gettime(time.CLOCK_MONOTONIC) - start_time
          if elapsed >= 3.0:
            sent = 1
-           print('Sending {0:s} at {1:0.3f}'.format(outstr, elapsed)) 
-           ser.write(outstr.encode())
+           print('Sending {0:s} at {1:0.3f}'.format(outstr1, elapsed)) 
+           ser.write(outstr1.encode())
+      if sent == 1:
+         elapsed = time.clock_gettime(time.CLOCK_MONOTONIC) - start_time
+         if elapsed >= 6.0:
+           sent = 2
+           print('Sending {0:s} at {1:0.3f}'.format(outstr2, elapsed))
+           ser.write(outstr2.encode())
+      if sent == 2:
+         elapsed = time.clock_gettime(time.CLOCK_MONOTONIC) - start_time
+         if elapsed >= 9.0:
+           sent = 3
+           print('Sending {0:s} at {1:0.3f}'.format(outstr3, elapsed))
+           ser.write(outstr3.encode())
       if got_fph:
         post_fph_count = post_fph_count + 1
         if post_fph_count > 5000:
